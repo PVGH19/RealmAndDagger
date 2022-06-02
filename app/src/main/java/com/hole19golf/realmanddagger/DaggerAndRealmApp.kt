1cd5c9cdae1abe6083795136dc7d4bbebdac5bb4
PVGH19 <pedro.gloria@hole19golf.com>
2022-06-02 11:06:33 +0100

    Some code cleanup

--- app/src/main/java/com/hole19golf/realmanddagger/DaggerAndRealmApp.kt
+++ app/src/main/java/com/hole19golf/realmanddagger/DaggerAndRealmApp.kt
@@ -6,28 +6,4 @@ import io.realm.Realm
 import io.realm.RealmConfiguration
 
 @HiltAndroidApp
-class DaggerAndRealmApp : Application() {
-
-    val realm: Realm
-        get() =  Realm.getDefaultInstance()
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-
-//        Realm.init(this)
-//
-//        val realmConfiguration = RealmConfiguration.Builder()
-//            .allowQueriesOnUiThread(true)
-//            .allowWritesOnUiThread(true)
-//            .build()
-//
-//        Realm.setDefaultConfiguration(realmConfiguration)
-    }
-
-    companion object {
-        @JvmStatic
-        lateinit var instance: DaggerAndRealmApp
-            private set
-    }
-}
+class DaggerAndRealmApp : Application()
